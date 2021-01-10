3854a0d824f0674eb9aeb3015ac7d42568a9a904
Bob-Loth <bob94loth@gmail.com>
2021-01-09 16:22:29 -0800

    made changes to characterData, added removeCharacter fn and delete button

--- src/Table.js
+++ src/Table.js
@@ -9,9 +9,7 @@ const TableHeader = () => {
        </tr>
      </thead>
    )
- }
- 
- 
+}
 
 const TableBody = props => {
    const rows = props.characterData.map((row, index) => {
@@ -19,23 +17,23 @@ const TableBody = props => {
          <tr key={index}>
             <td>{row.name}</td>
             <td>{row.job}</td>
+            <td>
+              <button onClick={() => props.removeCharacter(index)}>Delete</button>
+            </td>
          </tr>
       )
    })
    return <tbody>{rows}</tbody>
 }
 
-
-class Table extends Component {
-   render() {
-      const {characterData} = this.props
-     return (
-       <table>
-         <TableHeader />
-         <TableBody characterData={characterData}/>
-       </table>
-     )
-   }
+const Table = props => {
+  const { characterData, removeCharacter } = props
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
 }
 
 export default Table
